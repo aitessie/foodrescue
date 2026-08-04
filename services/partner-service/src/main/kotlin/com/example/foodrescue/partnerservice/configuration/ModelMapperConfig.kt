6bea8c9d6d585d160a2fb35b407ddf765a9fac3a
aitessie <yakovivan@mail.ru>
2026-08-04 16:33:45 +0300

    configure persistence for partner service

---
+++ services/partner-service/src/main/kotlin/com/example/foodrescue/partnerservice/configuration/ModelMapperConfig.kt
@@ -0,0 +1,18 @@
+package com.example.foodrescue.partnerservice.configuration
+
+import org.modelmapper.ModelMapper
+import org.modelmapper.convention.MatchingStrategies
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration(proxyBeanMethods = false)
+class ModelMapperConfig {
+
+    @Bean
+    fun modelMapper(): ModelMapper {
+        return ModelMapper().apply {
+            configuration.matchingStrategy =
+                MatchingStrategies.STRICT
+        }
+    }
+}
